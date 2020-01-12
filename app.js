26898a3843821c5bd2341a90b4478f906f05824a
Okwuidegbe Emmanuel <you@example.com>
2020-01-12 10:20:02 +0100

    Add bcrypt encryption

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const md5 = require("md5")
-
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 const app = express()
 const port = 3000
-
-console.log(process.env.API_KEY)
 
 app.set('view engine', 'ejs')
 
@@ -61,32 +59,36 @@ app.post("/register", (req, res) => {
     const userName = req.body.username
     const userPassword = req.body.password
 
-    const newUser = new User({
-        email: userName, 
-        password: md5(userPassword)
-    })
-
-    newUser.save(function(err){
-        if (!err) {
-            res.render("secrets")
-        } else {
-            console.log(err)
-        }
+    bcrypt.hash(userPassword, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: userName, 
+            password: hash
+        })
+    
+        newUser.save(function(err){
+            if (!err) {
+                res.render("secrets")
+            } else {
+                console.log(err)
+            }
+        })
     })
 })
 
 app.post("/login", (req, res) => {
     const userName = req.body.username
-    const userPassword = md5(req.body.password)
+    const userPassword = req.body.password
 
     User.findOne({email: userName}, function(err, foundUser){
         if (err) {
             console.log(err)
         } else {
             if (foundUser) {
-                if (foundUser.password === userPassword) {
-                    res.render("secrets")
-                }
+                bcrypt.compare(userPassword, foundUser.password, function(err, result){
+                    if (result) {
+                        res.render("secrets")
+                    }
+                })
             }
         }
     })
